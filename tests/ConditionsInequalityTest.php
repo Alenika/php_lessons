055ceee7b380b60e40b60a82a9ac4259996e9490
Elena <ahiyevichelena@gmail.com>
2025-09-24 23:24:28 +0800

    chore:sync test files after rename

--- tests/ConditionsInequalityTest.php
+++ tests/ConditionsInequalityTest.php
@@ -1,1 +1,16 @@
 <?php
+	declare(strict_types=1);
+	
+	use PHPUnit\Framework\TestCase;
+	
+	final class ConditionsInequalityTest
+	extends TestCase
+	{
+		public function
+		testOutputWhenNotEqual10(): void
+		{
+			$this -> expectOutputString('Переменная test не равна 10');
+			include __DIR__ .
+				'/../conditions/conditions_inequality.php';
+		}
+	}
