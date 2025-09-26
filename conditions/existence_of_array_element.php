13714de817ef62b650476178ef50a88d261206c9
Elena <ahiyevichelena@gmail.com>
2025-09-26 19:55:11 +0800

    feat: complete isset checks for array elements and vars

---
+++ conditions/existence_of_array_element.php
@@ -0,0 +1,24 @@
+<?php
+	/*
+	 * Проверка существования элемента массива в PHP
+	 */
+
+	// ⊗ppPmCdAEC №1
+	echo '+++';
+	// ⊗ppPmCdAEC №2
+	echo '+++';
+	// ⊗ppPmCdAEC №3
+	echo '+++';
+	// ⊗ppPmCdAEC №4
+	echo '+++';
+	echo 'Перем не объявлена, но isset подавит ворнинг, выведет false';
+	// ⊗ppPmCdAEC №5
+	echo '+++';
+	// ⊗ppPmCdAEC №6
+	echo '+++';
+	// ⊗ppPmCdAEC №7
+	echo '+++';
+	// ⊗ppPmCdAEC №8
+	echo 'Скорее всего ошибка'; 
+
+	
