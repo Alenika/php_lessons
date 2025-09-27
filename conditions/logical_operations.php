bf7a684f297b7a11695c831d1c5c2441a527d882
Elena <ahiyevichelena@gmail.com>
2025-09-27 20:38:26 +0800

    feat: logical op tasks

---
+++ conditions/logical_operations.php
@@ -0,0 +1,23 @@
+<?php
+	/*
+	 * Логические операции в PHP
+	 */
+
+	// ⊗ppPmCdLO №1
+	$a = 2 * (3 - 1);
+	$b = 6 - 2;
+	
+	var_dump($a == $b); // bool(true)
+	
+	// ⊗ppPmCdLO №2
+	$a = 5 * (7 - 4);
+	$b = 1 + 2 + 7;
+	
+	var_dump($a > $b); // bool(true)
+	
+	// ⊗ppPmCdLO №3
+	$a = 2 ** 4;
+	$b = 4 ** 2;
+	var_dump($a != $b); // bool(false)
+
+
