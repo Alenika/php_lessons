608b22a79612db1c7f231d83da45f59ccc719993
Elena <ahiyevichelena@gmail.com>
2025-09-28 21:33:28 +0800

    feat: task 1 seasons via match and switch

---
+++ conditions/if_else_practice.php
@@ -0,0 +1,38 @@
+<?php
+	/*
+	 * Практика на условия if-else в PHP
+	 */
+
+	// ⊗ppPmCdPrm
+
+	/* ---------- №1 ---------- */
+	$month = 12;
+	
+	$season = match ($month) {
+		12, 1, 2 => 'Зима ',
+		3, 4, 5 => 'Весна ',
+		6, 7, 8 => 'Лето ',
+		9, 10, 11 => 'Осень ',
+		default => 'Неверное значение'
+	};
+	echo $season . $month . PHP_EOL; // Зима 12
+
+	$month = 8;
+	
+	switch ($month) {
+		case 12: case 1: case 2:
+			echo 'Зима ';
+			break;
+		case 3: case 4: case 5:
+			echo 'Весна ';
+			break;
+		case 6: case 7: case 8:
+			echo 'Лето ';
+			break;
+		case 9: case 10: case 11:
+			echo 'Осень ';
+			break;
+		default:
+			echo 'Неверное значение';
+	}
+	echo $month . PHP_EOL; // Лето 8
