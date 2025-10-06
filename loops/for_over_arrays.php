161130b17f1843bf9fb8c8b055a0a56d8aed97c8
Elena <ahiyevichelena@gmail.com>
2025-10-06 16:24:00 +0800

    feat: or-over-array task №1

---
+++ loops/for_over_arrays.php
@@ -0,0 +1,16 @@
+<?php
+	/*
+	 * Цикл for для массивов в PHP
+	 */
+
+	// ⊗ppPmLpALF
+	
+	/* ----------  №1  ---------- */
+	
+	$arr = ['a', 'b', 'c', 'd', 'e'];
+	$length = count($arr);
+	
+	for ($i = 0; $i < $length; $i++) {
+		echo $arr[$i]; // abcde
+	}
+	
