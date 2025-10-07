e635fc9b80582db9a2aee8ec4dfbd2d2030f9f35
Elena <ahiyevichelena@gmail.com>
2025-10-07 22:37:07 +0800

    feat: loops practice task 13 +10% if salary <=400

--- loops/loops_practice.php
+++ loops/loops_practice.php
@@ -101,3 +101,20 @@
 		$arr[$key] = $elem + ($elem * 0.10);
 	}
 	var_dump($arr);
+	
+	// №13
+	$arr = [
+		'employee1' => 100,
+		'employee2' => 200,
+		'employee3' => 300,
+		'employee4' => 400,
+		'employee5' => 500,
+		'employee6' => 600,
+		'employee7' => 700,
+	];
+	foreach ($arr as $key => $elem) {
+		if ($elem <= 400) {
+			$arr[$key] = $elem + ($elem * 0.10);
+		}
+	}
+	var_dump($arr);
