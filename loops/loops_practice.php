2a57dc288373af2df55581bb5d3c07878df326d8
Elena <ahiyevichelena@gmail.com>
2025-10-07 22:34:08 +0800

    feat: loops practice task 12 +10% salary

--- loops/loops_practice.php
+++ loops/loops_practice.php
@@ -86,3 +86,18 @@
 		$factorial *= $i;
 	}
 	echo $factorial;
+	
+	// №12
+	$arr = [
+		'employee1' => 100,
+		'employee2' => 200,
+		'employee3' => 300,
+		'employee4' => 400,
+		'employee5' => 500,
+		'employee6' => 600,
+		'employee7' => 700,
+	];
+	foreach ($arr as $key => $elem) {
+		$arr[$key] = $elem + ($elem * 0.10);
+	}
+	var_dump($arr);
