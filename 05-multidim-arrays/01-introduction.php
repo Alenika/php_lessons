31d99fe6c81d8a5441568bf2336f2e7e5035f34b
Elena <ahiyevichelena@gmail.com>
2025-10-09 15:50:17 +0800

    multidim arrays task №2 sum all elements

--- 05-multidim-arrays/01-introduction.php
+++ 05-multidim-arrays/01-introduction.php
@@ -18,3 +18,20 @@
 	echo $arr[1][1];
 	echo $arr[2][0];
 	echo $arr[0][0];
+	
+	/* ---------- №2 ---------- */
+	$arr = [
+		[1, 2],
+		[3, 4],
+		[5, 6]
+	];
+	
+	$sum = 0;
+	
+	foreach ($arr as $arr_row) {
+		foreach ($arr_row as $elem) {
+			$sum += $elem;
+		}
+	}
+	
+	echo $sum; // 21
