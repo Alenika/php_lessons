c95e42bcf4625b54da2c17920c52ac38554904f2
Elena <ahiyevichelena@gmail.com>
2025-10-10 22:16:05 +0800

    feat: add 3D array iteration block

--- 05-multidim-arrays/05_iterating_multidim_arrays.php
+++ 05-multidim-arrays/05_iterating_multidim_arrays.php
@@ -1,1 +1,26 @@
 <?php
+	/*
+	 * Перебор многомерных массивов в PHP
+	 */
+
+	// ⊗ppPmMdIt
+
+	/* ------------- №1 ------------- */
+	
+	$arr = [
+		[1, 2, 3],
+		[4, 5, 6, 7],
+		[8, 9]
+	];
+	
+	$sum = 0;
+	
+	foreach ($arr as $sub2) {
+		foreach ($arr as $sub3) {
+			foreach ($sub3 as $elem) {
+				$sum +=$elem;
+			}
+		}
+	}
+	
+	echo $sum; // 135
