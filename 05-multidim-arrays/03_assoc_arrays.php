1768929890d2407eb135e0d1acd64b671c4d9e1b
Elena <ahiyevichelena@gmail.com>
2025-10-10 21:18:40 +0800

    feat: assoc arrays task №1

---
+++ 05-multidim-arrays/03_assoc_arrays.php
@@ -0,0 +1,24 @@
+<?php
+	/*
+	 * Ассоциативные массивы в PHP
+	 */
+
+	// ⊗ppPmMdAs
+	
+	/* ---------- №1 ---------- */
+
+	$arr = [
+		'boys'=>[
+			1=>'John',
+			2=>'Jack',
+			3=>'Ryan'
+		],
+		'girls'=>[
+			1=>'Emma',
+			2=>'Lily',
+			3=>'Anna'
+		],
+	];
+	
+	echo $arr['boys'][1]; // John
+	echo $arr['girls'][2]; // Lily
