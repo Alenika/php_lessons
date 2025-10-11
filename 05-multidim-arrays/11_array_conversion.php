f83d1f66c5785277cc879314a9bf850f90467dca
Elena <ahiyevichelena@gmail.com>
2025-10-11 15:16:04 +0800

    feat: group cities by country task 1, group events by date task 2, flatten events back to flat list task 3

---
+++ 05-multidim-arrays/11_array_conversion.php
@@ -0,0 +1,121 @@
+<?php
+	/*
+	 * Конвертация многомерных массивов в PHP
+	 */
+
+	// ⊗ppPmMdCn
+
+	// №1
+	
+	$arr = [
+		[
+			'country' => 'country1',
+			'city' =>    'city1',
+		],
+		[
+			'country' => 'country2',
+			'city' =>    'city2',
+		],
+		[
+			'country' => 'country1',
+			'city' =>    'city3',
+		],
+		[
+			'country' => 'country1',
+			'city' =>    'city4',
+		],
+		[
+			'country' => 'country3',
+			'city' =>    'city5',
+		],
+		[
+			'country' => 'country2',
+			'city' =>    'city6',
+		],
+		[
+			'country' => 'country3',
+			'city' =>    'city7',
+		],
+	];
+	
+	$res = [];
+	
+	foreach ($arr as $elem) {
+		$country = $elem['country'];
+		$city = $elem['city'];
+		$res[$country][$city] = $city;
+	}
+	
+	var_dump($res);
+	
+	// №2
+
+	$arr = [
+		[
+			'date'  => '2019-12-29',
+			'event' => 'name1'
+		],
+		[
+			'date'  => '2019-12-31',
+			'event' => 'name2'
+		],
+		[
+			'date'  => '2019-12-29',
+			'event' => 'name3'
+		],
+		[
+			'date'  => '2019-12-30',
+			'event' => 'name4'
+		],
+		[
+			'date'  => '2019-12-29',
+			'event' => 'name5'
+		],
+		[
+			'date'  => '2019-12-31',
+			'event' => 'name6'
+		],
+		[
+			'date'  => '2019-12-29',
+			'event' => 'name7'
+		],
+		[
+			'date'  => '2019-12-30',
+			'event' => 'name8'
+		],
+		[
+			'date'  => '2019-12-30',
+			'event' => 'name9'
+		],
+	];
+	
+	$res = [];
+	
+	foreach ($arr as $elem) {
+		$res[$elem['date']][] = $elem['event'];
+	}
+	
+	var_dump($res);
+	
+	// №3
+
+	$arr = [
+		'2019-12-29'=> ['name1', 'name2', 'name3',
+			'name4'],
+		'2019-12-30'=> ['name5', 'name6',
+			'name7'],
+		'2019-12-31'=> ['name8', 'name9'],
+	];
+	
+	$res = [];
+	
+	foreach ($arr as $date => $events) {
+		foreach ($events as $event) {
+			$res[] = [
+				'date' => $date,
+				'event' => $event,
+			];
+		}
+	}
+	
+	var_dump($res);
