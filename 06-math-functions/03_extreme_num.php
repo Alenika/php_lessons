957c96f01cebeb7637f5fa3b87092cbd87a750f2
Elena <ahiyevichelena@gmail.com>
2025-10-15 13:48:34 +0800

    math functions task 1 extreme numbers min max

---
+++ 06-math-functions/03_extreme_num.php
@@ -0,0 +1,14 @@
+<?php
+	/*
+	 * ⊗ppPmMFEx
+	 */
+
+	// №1
+	
+	$arr = [4, -2, 5, 19, -130, 0, 10];
+	
+	$min_num = min($arr);
+	$max_num = max($arr);
+	
+	echo $min_num . '<br>'; // -130
+	echo $max_num . '<br>'; // 19
