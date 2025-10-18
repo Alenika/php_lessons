ae003d3b17009f69470352f0b9df66492326ea04
Elena <ahiyevichelena@gmail.com>
2025-10-18 14:07:08 +0800

    str_repeat()

---
+++ 07-string-functions/11_repeat.php
@@ -0,0 +1,17 @@
+<?php
+	/*
+	 * Заполнение строки в PHP
+	 */
+
+	// ⊗ppPmSFFl
+	
+	// №1
+	for ($i = 0; $i < 10; $i++) {
+		echo str_repeat('x', $i) . PHP_EOL;
+	}
+	
+	// №2
+	for ($i = 1; $i < 10; $i++) {
+		echo str_repeat($i, $i) . PHP_EOL;
+	}
+	
