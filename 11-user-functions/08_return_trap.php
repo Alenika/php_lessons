d91acdda25b3f8999c5ce045edf685f3d1168608
Elena <ahiyevichelena@gmail.com>
2025-10-25 09:36:27 +0800

    fix: rename functions

--- 11-user-functions/08_return_trap.php
+++ 11-user-functions/08_return_trap.php
@@ -6,17 +6,17 @@
 	// ⊗ppPmUFRN
 	
 	/* ------------- №1 ------------- */
-	function func($num) {
+	function func8_1($num) {
 		return $num; // выполнение функции прекращается
 		
 		$res = $num * $num; // этот код не будет выполнен
 		return $res;
 	}
 	
-	echo func(3); // 3
+	echo func8_1(3); // 3
 	
 	/* ------------- №2 ------------- */
-	function func($num) {
+	function func8_2($num) {
 		if ($num <= 0) {
 			return abs($num); // возвращает абсолютное значение
 		} else {
@@ -24,8 +24,8 @@
 		}
 	}
 	
-	echo func(10); // 100 - выполняется блок else
-	echo func(-5); // 5 - выполняется блок if
+	echo func8_2(10); // 100 - выполняется блок else
+	echo func8_2(-5); // 5 - выполняется блок if
 	
 	/* ------------- №3 ------------- */
 	function func($num) {
