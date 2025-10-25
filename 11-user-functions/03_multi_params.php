2e5b6bf0caa7cb5d6df79e80e529e176a40d1fd0
Elena <ahiyevichelena@gmail.com>
2025-10-25 08:29:31 +0800

    feat: Add function supporting multip params (calculate_sum_3)

---
+++ 11-user-functions/03_multi_params.php
@@ -0,0 +1,14 @@
+<?php
+	/*
+	 * Multiple Function Parameters in PHP
+	 */
+	
+	// ⊗ppPmUFSP
+	
+	/* ------------- №1 ------------- */
+	function calculate_sum_3 ($num1, $num2, $num3) {
+		echo $num1 + $num2 + $num3 . PHP_EOL;
+	}
+	
+	calculate_sum_3(1, 2, 3); // 6
+	calculate_sum_3('1', '2', '3'); // 6
