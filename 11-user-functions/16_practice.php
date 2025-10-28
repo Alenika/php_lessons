d715d4d6cb3893ba34a08737c11ff8601029c75e
Elena <ahiyevichelena@gmail.com>
2025-10-28 17:56:35 +0800

    feat: add practice.php

---
+++ 11-user-functions/16_practice.php
@@ -0,0 +1,44 @@
+<?php
+	/*
+	 * Практика на функции PHP
+	 */
+	
+	// ⊗ppPmUFPrm
+	
+	/* ------------- №1 ------------- */
+	function get_divisors($num) {
+		$arr = [];
+		for ($i = 1; $i <= $num; $i++) {
+			if ($num % $i === 0) {
+				$arr[] = $i;
+			}
+		}
+		return $arr;
+	}
+	
+	print_r(get_divisors(2)); // [0] => 1  [1] => 2
+	print_r(get_divisors(3)); // [0] => 1  [1] => 3
+	
+	/* ------------- №2 ------------- */
+	function common_divisors($num1, $num2) {
+		return array_intersect(get_divisors($num1, $num2), get_divisors($num2));
+	}
+	
+	print_r(common_divisors(10, 20)); // [0] => 1 [1] => 2 [2] => 5 [3] => 10
+	
+	/* ------------- №3 ------------- */
+	function calculate_digit_sum($num) {
+		$sum = 0;
+		$str_number = (string)abs($num);
+		
+		for ($i = 0; $i < strlen($str_number); $i++) {
+			$sum += (int)$str_number[$i];
+		}
+		return $sum;
+	}
+	
+	print_r(calculate_digit_sum(150)); // 6
+
+	
+
+
