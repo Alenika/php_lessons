13ab07fec0c91d1f8d730fd230f6a8e29012ecc6
Elena <ahiyevichelena@gmail.com>
2025-12-04 22:03:42 +0800

    Add foreach example with inserting array elements into strings

---
+++ 15-string-formatting/05_loop_arr_insert.php
@@ -0,0 +1,13 @@
+<?php
+	/*
+	 * Inserting Array Elements in a Loop in PHP
+	 */
+
+	// ⊗ppPmSFLArI
+	
+	/* ------------- №1 ------------- */
+	$arr = ['a' => 1, 'b' => 2, 'c' => 3];
+	
+	foreach ($arr as $key => $elem) {
+		echo "pair: $elem $key<br>";
+	}
