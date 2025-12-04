55e63c36d972a349fc298051a20e51780e5591a7
Elena <ahiyevichelena@gmail.com>
2025-12-04 21:47:46 +0800

    Add examples for inserting array items into strings

---
+++ 15-string-formatting/02_array_item_insert.php
@@ -0,0 +1,13 @@
+<?php
+	/*
+	 * Inserting Array Elements in PHP
+	 */
+
+	// ⊗ppPmSFAEI
+	
+	/* ------------- №1 ------------- */
+	$arr = ['1', '2', '3'];
+	echo 'aaa ' . $arr[0] . ' bbb ' . $arr[1] . PHP_EOL; // aaa 1 bbb 2
+	
+	// Simplify
+	echo "ааа {$arr[0]} bbb {$arr[1]}"; // ааа 1 bbb 2
