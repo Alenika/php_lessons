5d8872a136465502181485b52082d2c1939404f0
Elena <ahiyevichelena@gmail.com>
2025-12-04 22:00:13 +0800

    Add loop examples with variable insertion into strings

--- 15-string-formatting/04_loop_var_insert.php
+++ 15-string-formatting/04_loop_var_insert.php
@@ -1,1 +1,13 @@
 <?php
+	/*
+	 * Loop and Variable Interpolation in PHP
+	 */
+
+	// ⊗ppPmSFLVI
+	
+	/* ------------- №1 ------------- */
+	for ($i = 1; $i <= 10; $i++) {
+		for ($j = 1; $j <= 10; $j++) {
+			echo "nums: $i $j<br>";
+		}
+	}
