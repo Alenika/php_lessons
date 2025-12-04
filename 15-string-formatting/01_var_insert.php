27b343d643823983a9b9863343fc4f881a55e235
Elena <ahiyevichelena@gmail.com>
2025-12-04 21:39:25 +0800

    Add basic example for string variable insertion

---
+++ 15-string-formatting/01_var_insert.php
@@ -0,0 +1,13 @@
+<?php
+	/*
+	 * Inserting variables into strings in PHP
+	 */
+
+    // ⊗ppPmSFVSI
+	
+	/* ------------- №1 ------------- */
+	$name = 'user';
+	echo 'hello, ' . $name . '!' . PHP_EOL; // hello, user!
+
+	// Simplify
+	echo "hello, $name!"; // hello, user!
