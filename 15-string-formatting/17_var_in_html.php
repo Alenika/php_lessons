63b94edd58d814313f42f897c8253f1287060cb3
Elena <ahiyevichelena@gmail.com>
2025-12-05 22:05:24 +0800

    Add examples of inserting PHP variables into HTML paragraphs

---
+++ 15-string-formatting/17_var_in_html.php
@@ -0,0 +1,18 @@
+<?php
+	/*
+	 * Inserting a PHP Variable into HTML Code
+	 */
+
+	// ⊗ppPmSFVO
+	
+	/* ------------- №1 ------------- */
+	$str1 = 'text1';
+	$str2 = 'text2';
+	$str3 = 'text3';
+	
+	/*
+	 * <p><?= $str1 ?></p>
+	 * <p><?= $str2 ?></p>
+	 * <p><?= $str3 ?></p>
+	 */
+	
