6b8624fc1b22af1cf69df78afb3055098c4057d1
Elena <ahiyevichelena@gmail.com>
2025-12-05 14:41:20 +0800

    Add img tag generation examples with attributes

---
+++ 15-string-formatting/08_tag_gen_attr.php
@@ -0,0 +1,15 @@
+<?php
+	/*
+	 * Generating Tags with Attributes in PHP
+	 */
+
+	// ⊗ppPmSFTA
+	
+	/* ------------- №1 ------------- */
+	$src1 = '1.png';
+	$src2 = '2.png';
+	$src3 = '3.png';
+	
+	echo "<img src=\"$src1\">" . PHP_EOL;
+	echo "<img src=\"$src2\">" . PHP_EOL;
+	echo "<img src=\"$src3\">" . PHP_EOL;
