7b478ed9c7d6652cbf7c6824beddc97d1f74c4c7
Elena <ahiyevichelena@gmail.com>
2025-12-05 15:41:19 +0800

    Imlement dynamic HTML table generation from an array of assoc arrays

--- 15-string-formatting/12_html_table.php
+++ 15-string-formatting/12_html_table.php
@@ -1,1 +1,23 @@
 <?php
+	/*
+	 * Loop and HTML Table Generation in PHP
+	 */
+
+	// ⊗ppPmSFLT
+	
+	/* ------------- №1 ------------- */
+	$arr = [
+		['name' => 'user1', 'age' => 30, 'salary' => 500],
+		['name' => 'user2', 'age' => 31, 'salary' => 600],
+		['name' => 'user3', 'age' => 32, 'salary' => 700],
+	];
+	
+	echo '<table>' . PHP_EOL;
+	foreach ($arr as $item) {
+			echo '<tr>' . PHP_EOL;
+				echo "<td>{$item['name']}</td>" . PHP_EOL;
+				echo "<td>{$item['age']}</td>" . PHP_EOL;
+				echo "<td>{$item['salary']}</td>" . PHP_EOL;
+			echo '</tr>' . PHP_EOL;
+	}
+	echo '</table>' . PHP_EOL;
