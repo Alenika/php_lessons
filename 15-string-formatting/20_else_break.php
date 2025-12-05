e580a25f39a99c125d3ea8dce4b0a4ae9f0d5e7e
Elena <ahiyevichelena@gmail.com>
2025-12-05 22:23:37 +0800

    Add if-else condition with HTML blocks breaking

---
+++ 15-string-formatting/20_else_break.php
@@ -0,0 +1,26 @@
+<?php
+	/*
+	 * Else Block in Conditions for Breaking PHP Code
+	 */
+
+	// ⊗ppPmSFEB
+	
+	/* ------------- №1 ------------- */
+	$show = true;
+	
+	/*
+	 * <?= if ($show): ?>
+	 * <div>
+	 *  <p>text+</p>
+	 *  <p>text+</p>
+	 *  <p>text+</p>
+	 * </div>
+	 *
+	 * <?= else: ?>
+	 * <div>
+	 *  <p>text-</p>
+	 *  <p>text-</p>
+	 *  <p>text-</p>
+	 * </div>
+	 *<?= endif: ?>
+	 */
