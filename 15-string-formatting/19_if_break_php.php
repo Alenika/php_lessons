4f06909bf8d3bcee8e0c114010331bb1120cfe48
Elena <ahiyevichelena@gmail.com>
2025-12-05 22:16:14 +0800

    Add if condition with HTML code breaking

---
+++ 15-string-formatting/19_if_break_php.php
@@ -0,0 +1,19 @@
+<?php
+	/*
+	 * Conditions and Breaking PHP Code
+	 */
+
+	// ⊗ppPmSFIB
+	
+	/* ------------- №1 ------------- */
+	$show = true;
+	
+	/*
+	 * <?php if ($show): ?>
+	 * <div>
+	 * <p>text1</p>
+	 * <p>text2</p>
+	 * <p>text3</p>
+	 * </div>
+	 * <?php endif; ?>
+	 */
