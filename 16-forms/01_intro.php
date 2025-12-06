748df95b76b409f2a5625afcb587dedd0dd8b762
Elena <ahiyevichelena@gmail.com>
2025-12-06 21:25:32 +0800

    Add basic user form with name, age, and salary

---
+++ 16-forms/01_intro.php
@@ -0,0 +1,24 @@
+<?php
+	/*
+	 * Basics of Working with Forms in PHP
+	 */
+	
+	// ⊗ppPmFmInr
+	
+	/* ------------- №1 ------------- */
+	
+	?>
+
+	<form>
+		<label for="name">Имя</label>
+		<input id="name" type="text"   name="name"   placeholder="Имя">
+		
+		<label for="age">Возраст</label>
+		<input id="age" type="number" name="age"    placeholder="Возраст">
+		
+		<label for="salary">Зарплата</label>
+		<input id="salary" type="number" name="salary" placeholder="Зарплата">
+		
+		<input type="submit" value="Отправить">
+	</form>
+
