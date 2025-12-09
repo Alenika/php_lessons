e25582516d6630ee8b629992b3b6845183b4e291
Elena <ahiyevichelena@gmail.com>
2025-12-09 15:59:01 +0800

    feat(forms): Implement form handling with GET method to retrieve and sum user-enetered numeric data

--- 16-forms/04_get_method.php
+++ 16-forms/04_get_method.php
@@ -1,1 +1,19 @@
 <?php
+	/*
+	 * Retrieving Form Data Using the GET Method in PHP
+	 */
+
+	// ⊗ppPmFmDGEG
+	
+	/* ------------- №1 ------------- */
+
+?>
+
+<form action="04_result.php" method = "GET">
+	<input type = "text" name = "number1" placeholder = "Число 1">
+	<input type = "text" name = "number2" placeholder = "Число 2">
+	<input type = "text" name = "number3" placeholder = "Число 3">
+	<input type = "submit" value = "Отправить GET">
+</form>
+
+
--- 16-forms/04_result.php
+++ 16-forms/04_result.php
@@ -1,1 +1,16 @@
 <?php
+	// ⊗ppPmFmDGEG
+	
+	/* ------------- №1 ------------- */
+	
+	if (isset($_GET['number1'])) {
+		$num1 = (float)$_GET['number1'];
+		$num2 = (float)$_GET['number2'];
+		$num3 = (float)$_GET['number3'];
+		
+		$sum = $num1 + $num2 +$num3;
+		
+		echo "Сумма введенных чисел: " . $sum;
+	} else {
+		echo "Числа не заполнены.";
+	}
