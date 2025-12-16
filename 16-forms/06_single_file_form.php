c4f5eb528e80a957b3e8411ae0be52d903f78884
Elena <ahiyevichelena@gmail.com>
2025-12-16 21:16:38 +0800

    feat(forms): Implemented self-submitting POST form for rectangle area calculation

--- 16-forms/06_single_file_form.php
+++ 16-forms/06_single_file_form.php
@@ -9,10 +9,15 @@
 
 	/* ------------- №1 ------------- */
 	<form action="" method="POST">
-	<input name="surname">
-	<input name="name">
-	<input name="middle">
-	<input type="submit">
+		<label for="surname">Введите фамилию:</label>
+		<input id="surname" type="text" name="surname" placeholder="Макаревич">
+		<br><br>
+		<label for="name">Введите имя:</label>
+		<input id="name" type="text" name="name" placeholder="Михаил">
+		<br><br>
+		<label for="middle_name">Введите отчество</label>
+		<input id="middle_name" type="text" name="middle" placeholder="Антонович">
+		<input type="submit" value="Отправить">
 	</form>
 	
 <?php
@@ -21,6 +26,28 @@
 		$name = $_POST['name'];
 		$middle = $_POST['middle'];
 		
-		echo $surname . " " . $name . " " . $middle;
+		echo "Меня зовут $surname $name $middle";
 	}
 ?>
+
+<h3>Калькулятор площади</h3>
+
+<form action="" method="post">
+	<label for="length">Введите длину прямоугольника:</label>
+	<input id="length" type="number" name="length" placeholder="2">
+	<br><br>
+	<label for="width">Введите ширину прямоугольника:</label>
+	<input id="width" type="number" name="width" placeholder="4">
+	<br><br>
+	<input type="submit" value="Рассчитать площадь">
+</form>
+
+<?php
+	if (isset($_POST['length'])) {
+		$length = $_POST['length'];
+		$width = $_POST['width'];
+		
+		$rectangle_area = $length * $width;
+		
+		echo "Площадь прямоугольника = $rectangle_area";
+	}
