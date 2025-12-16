89033928e84ee5974887c6ed26058a714dbb7a47
Elena <ahiyevichelena@gmail.com>
2025-12-16 16:32:18 +0800

    refactor(forms/accessibility): Improved form structure by adding explicit <label for='ID'> associations to all input fields

--- 16-forms/03_get_data.php
+++ 16-forms/03_get_data.php
@@ -8,16 +8,16 @@
 	/* ------------- №1 ------------- */
 ?>
 
-<form action = "03_result_get.php" method="get">
-	<label>Ваш возраст:</label>
-	<input type="text" name="age">
-	<input type="submit" value="Отправить GET">
+<form action="/03_result_get.php" method="get">
+	<label for="03_age">Ваш возраст:</label>
+	<input id="03_age" type="text" name="age" placeholder="Возраст">
+	<input type="submit" value="Отправить (GET)">
 </form>
 
-<form action = "03_result_post.php" method = "POST">
-	<label>Ваше имя:</label>
-	<input type = "text" name = "name">
-	<input type = "submit" value="Отправить POST">
+<form action="/03_result_post.php" method="post">
+	<label for="03_name">Ваше имя:</label>
+	<input id="03_name" type="text" name="name" placeholder="Имя">
+	<input type="submit" value="Отправить (POST)">
 </form>
 
 
