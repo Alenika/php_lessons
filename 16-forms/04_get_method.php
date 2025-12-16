b7307d7c104c41eb5c4bd1eaa92d124f4c03c1d3
Elena <ahiyevichelena@gmail.com>
2025-12-16 17:38:37 +0800

    refactor(forms): Form structure refinement for GET method, replacing absolute path in action and ensuring accessibility with <label for='ID' attributes

--- 16-forms/04_get_method.php
+++ 16-forms/04_get_method.php
@@ -9,10 +9,16 @@
 
 ?>
 
-<form action="04_result.php" method = "GET">
-	<input type = "text" name = "number1" placeholder = "Число 1">
-	<input type = "text" name = "number2" placeholder = "Число 2">
-	<input type = "text" name = "number3" placeholder = "Число 3">
+<form action="/04_result.php" method = "GET">
+	<label for="number1">Первое число</label>
+		<input id="number1" type = "text" name = "number1" placeholder = "Число 1">
+	<br><br>
+	<label for="number2">Второе число</label>
+		<input id="number2" type = "text" name = "number2" placeholder = "Число 2">
+	<br><br>
+	<label for="number3">Третье число</label>
+		<input id="number3" type = "text" name = "number3" placeholder = "Число 3">
+	<br><br>
 	<input type = "submit" value = "Отправить GET">
 </form>
 
