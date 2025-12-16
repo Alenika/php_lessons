cd7a5878b859d8b38a33ce998fa03f58fcb1aa98
Elena <ahiyevichelena@gmail.com>
2025-12-16 22:06:05 +0800

    feat(forms): Implemented form hiding logic

--- 16-forms/07_hide_after_submit.php
+++ 16-forms/07_hide_after_submit.php
@@ -6,16 +6,39 @@
 	// ⊗ppPmFmHAS
 	
 	/* ------------- №1 ------------- */
-
+	// Первый заход (нет данных), условие истинно, показывает форму
 	if (empty($_GET)) {
 ?>
-
+<h3>/* ------------- №1 ------------- */</h3>
 <form action = "" method = "GET">
-	<input type = "text" name = "name" placeholder = "Ваше имя?">
+	<label for="name">Как вас зовут?</label>
+	<input id="name" type="text" name="name" placeholder="Антон">
 	<input type = "submit" value = "Отправить">
 </form>
 
 <?php
+	// После отправки(есть данные), условие if (empty($_POST)) ложно
+	// Пропускает форму и выполняет код в блоке else
 	} else {
-		echo $_GET['name'];
+		echo "Привет, {$_GET['name']}";
 	}
+?>
+
+<?php
+	if (isset($_POST['email'])) {
+?>
+
+<h3>/* ------------- №2 ------------- */</h3>
+<h3>Скрытие сброса пароля</h3>
+
+<form action="" method="post">
+	<label for="email">Введите ваш Email</label>
+	<input id="email" type="email" name="email">
+	<input type="submit" value="Отправить">
+</form>
+
+<?php
+	} else {
+		echo "Инструкция по сбросу пароля отправлена на почту: {$_POST['email']}";
+	}
+?>
