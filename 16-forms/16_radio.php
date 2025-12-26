4c3a4e4a02acd1a0e66955a589e240a3b8182a98
Elena <ahiyevichelena@gmail.com>
2025-12-26 21:08:22 +0800

    feat(forms): add complex form with text, radio and persistent checkboxes

--- 16-forms/16_radio.php
+++ 16-forms/16_radio.php
@@ -42,3 +42,59 @@ if (isset($_POST['gender'])) {
 <?php if ($result): ?>
 	<p><?= $result ?></p>
 <?php endif; ?>
+
+<hr>
+<h3>Регистрация на митап</h3>
+
+<?php
+	$message = null;
+	
+	if (!empty($_POST)) {
+		if (!empty($_POST['name'])) {
+			$name = $_POST['name'];
+			$level = $_POST['level'];
+			$mailing = $_POST['mailing'] ? "подписан" : "не подписан";
+			
+			$message = "Привет, $name! Твой уровень $level успешно зарегистрирован. На рассылку $mailing";
+		} else {
+			$message = "Введите имя";
+		}
+	}
+	
+?>
+
+<form action="" method="post">
+	<label for="name">Введите имя:</label>
+	<input id="name" type="text" name="name" value="<?= htmlspecialchars($_POST['name']) ?? '' ?>">
+	
+	<br><br>
+	
+	<input type="hidden" name="level" value="none">
+
+	<input id="junior" type="radio" name="level" value="junior"
+	<?php if (($_POST['level'] ?? 'junior') === 'junior') echo 'checked'; ?>>
+	<label for="junior">Junior</label>
+	<br>
+	<input id="middle" type="radio" name="level" value="middle"
+	<?php if (($_POST['level'] ?? '') === 'middle') echo 'checked'; ?>>
+	<label for="middle">Middle</label>
+	<br>
+	<input id="senior" type="radio" name="level" value="senior"
+	<?php if (($_POST['level'] ?? '') === 'senior') echo 'checked'; ?>>
+	<label for="senior">Senior</label>
+	
+	<br><br>
+	
+	<input type="hidden" name="mailing" value="0">
+	<input id="mailing" type="checkbox" name="mailing" value="1"
+	<?php if (!empty($_POST['mailing'])) echo 'checked' ?>>
+	<label for="mailing">Хочу получать анонсы новых митапов</label>
+
+	<br><br>
+	
+	<input type="submit" value="Отправить">
+</form>
+
+<?php if ($message): ?>
+	<p><?= $message ?></p>
+<?php endif; ?>
