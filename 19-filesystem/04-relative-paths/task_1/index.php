6e935c4325a43e46159fb173fffa53e26dee1981
Elena <ahiyevichelena@gmail.com>
2026-01-04 22:02:22 +0800

    feat(fs): solve relative paths tasks (navigating with ../)

---
+++ 19-filesystem/04-relative-paths/task_1/index.php
@@ -0,0 +1,11 @@
+<?php
+	/*
+	 * Относительные пути в PHP
+	 * ⊗ppPmFSRP
+	 * 334 of 447
+	 */
+
+	echo "Задача 1: " . file_get_contents('dir1/dir2/test.txt');
+	// Задача 1: Относительные пути в PHP
+
+	
---
+++ 19-filesystem/04-relative-paths/task_3/script1/script2/index.php
@@ -0,0 +1,8 @@
+<?php
+	/*
+	 * ⊗ppPmFSRP
+	 * Задача 3
+	 */
+
+	echo file_get_contents('../../dir/test.txt');
+	// 12345678
