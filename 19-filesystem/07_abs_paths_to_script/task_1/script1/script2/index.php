44e0dedcd376f0f911bfa3a2244f8b1e1dc7e060
Elena <ahiyevichelena@gmail.com>
2026-01-09 16:23:06 +0800

    feat(fs): modified task 19 move file and create dir

--- 19-filesystem/07_abs_paths_to_script/task_1/script1/script2/index.php
+++ 19-filesystem/07_abs_paths_to_script/task_1/script1/script2/index.php
@@ -1,1 +1,9 @@
 <?php
+	/*
+	 * Абсолютные пути к папке и файлу скрипта в PHP
+	 * ⊗ppPmFSSAP
+	 * 337 of 447
+	 */
+	
+	$path = __DIR__ . '/../../dir/test.txt';
+	echo file_get_contents($path);
--- 19-filesystem/09_move_file/task_1/index.php
+++ 19-filesystem/09_move_file/task_1/index.php
@@ -1,1 +1,9 @@
 <?php
+	/*
+	 * Перемещение файлов в PHP
+	 * ⊗ppPmFSFMv
+	 * 339 of 447
+	 * №1
+	 */
+
+	rename('file.txt', 'dir/file.txt');
