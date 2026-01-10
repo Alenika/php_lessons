8646a2795fba3cc713450a1ee4cfdfd6c78dd6b9
Elena <ahiyevichelena@gmail.com>
2026-01-10 23:26:24 +0800

    Implement file writing and appending using file_put_contents

---
+++ 19-filesystem/27_practice_test/03_writing.php
@@ -0,0 +1,22 @@
+<?php
+	echo "<h3>Запись файлов</h3>";
+	
+	$filePath = __DIR__ . '/file_manager_test/test.txt';
+	
+	// 1. Запись всего файла (перезапись)
+	$content = "Это первая строка.\nЭто вторая строка.\nЭто третья строка.";
+	
+	if (file_exists($filePath)) {
+		echo "<p>Файл НАЙДЕН!</p>";
+		file_put_contents($filePath, $content);
+		echo file_get_contents($filePath) . "<br>";
+	} else {
+		echo "<p style='color: #a40000'>Файл НЕ НАЙДЕН!</p>";
+	}
+	
+	// Дозапись в конец файла
+	$additionalContent = "\nЭто четвертая строка";
+	file_put_contents($filePath, $additionalContent, FILE_APPEND);
+	echo file_get_contents($filePath);
+	
+	
