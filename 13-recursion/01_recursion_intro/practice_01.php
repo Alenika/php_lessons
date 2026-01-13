f53bb2d0b26fd5722634b9acb866f947920b520d
Elena <ahiyevichelena@gmail.com>
2026-01-13 16:52:41 +0800

    feat(recursion): implement sum of digits using modulo and division

--- 13-recursion/01_recursion_intro/practice_01.php
+++ 13-recursion/01_recursion_intro/practice_01.php
@@ -42,4 +42,52 @@
 	printEvens(10);
 	
 	
+	echo "<h4>3. Факториал числа</h4>";
+	// Произведение всех чисел от 1 до n
+	
+	function factorial(int $n): int {
+		
+		if ($n <= 1) {
+			return 1;
+		}
+		
+		return $n * factorial($n - 1);
+	}
+	
+	echo factorial(5);
+	
+	
+	echo "<h4>4. Длина строки</h4>";
+	
+	function myStrLen(string $str): int {
+		if ($str === "") {
+			return 0;
+		}
+		
+		$remainder = mb_substr($str, 1);
+		
+		return 1 + myStrLen($remainder);
+	}
+	
+	echo "Длина строки: " . myStrLen("Hello");
+	
+	
+	echo "<h4>5. Сумма цифр числа</h4>";
+	
+	function sumDigits(int $n): int {
+		if ($n === 0) {
+			return 0;
+		}
+		
+		$lastDigit = $n % 10;
+		
+		$remainingPart = (int)($n / 10);
+		
+		return $lastDigit + sumDigits($remainingPart);
+	}
+ 
+	echo "Сумма цифр числа 123: " . sumDigits(123);
+	echo "<br>";
+	echo "Сумма цифр числа 505: " . sumDigits(505);
+	
 	
