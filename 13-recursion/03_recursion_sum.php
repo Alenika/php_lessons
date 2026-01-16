93f9166ad66bf1196223f1737183ccf623bd70ea
Elena <ahiyevichelena@gmail.com>
2026-01-16 14:56:42 +0800

    style: refactor getSum to PSR-12 and add type hinting

--- 13-recursion/03_recursion_sum.php
+++ 13-recursion/03_recursion_sum.php
@@ -1,21 +1,24 @@
 <?php
-	/*
+	/**
 	 * Сумма элементов массива при рекурсии в PHP
+	 * ⊗ppPmRcAS
 	 */
-
-	// ⊗ppPmRcAS
+	
+	declare(strict_types=1);
 	
 	/* ------------- №1 ------------- */
+	echo "<h3>1. Сумма элементов массива при рекурсии в PHP.</h3>";
+	
 	$arr = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5];
 	
-	function get_sum($array) {
+	function getSum($array) {
 		$sum = array_shift($array);
 		
 		if (count($array) !== 0) {
-			$sum += get_sum($array);
+			$sum += getSum($array);
 		}
 		
 		return $sum;
 	}
 	
-	var_dump(get_sum($arr));
+	var_dump(getSum($arr));
