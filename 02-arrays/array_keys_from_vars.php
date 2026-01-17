0baca25ebb50b5a972c3b76eec9e06c98491daa8
Elena <ahiyevichelena@gmail.com>
2026-01-17 16:55:08 +0800

    feat: access array elements using variables as keys (review)

--- 02-arrays/array_keys_from_vars.php
+++ 02-arrays/array_keys_from_vars.php
@@ -1,22 +1,38 @@
 <?php
-	/*
+	declare(strict_types=1);
+	
+	/**
 	 * Ключи из переменных в PHP
+	 * Динамический доступ к элементам массива
+	 * ⊗ppPmArVK | 65 of 447
 	 */
-
-	// ⊗ppPmArVK №1
+	
+	// --- Теория ---
+	$letters = ['a', 'b', 'c'];
+	$index = 2;
+	echo "Элемент под индексом {$letters[$index]} " . $letters[$index];
+	echo "<hr>";
+	
+	/**
+	 * ПРАКТИКА
+	 */
+	
+	// ⊗ppPmArVK №1: Сумма элементов по ключам из переменных
 	$arr = [1, 2, 3, 4, 5];
 	$key1 = 1;
 	$key2 = 2;
 	
 	$sum = $arr[$key1] + $arr[$key2];
-	echo $sum; // 5
+	echo "Сумма элементов с ключами {$key1} и {$key2} равна: {$sum}";
+	echo "<br>";
 	
-	// ⊗ppPmArVK №2
+	// ⊗ppPmArVK №2: Ассоциативный доступ через переменную
 	$arr = [
 		'a' => 1,
 		'b' => 2,
-		'c' => 3
-	];
+		'c' => 3,
+		];
+	
 	$key = 'b';
-	$value = $arr[$key];
-	echo $value;
+	
+	echo "Элемент с ключом '{$key}': " . $arr[$key];
