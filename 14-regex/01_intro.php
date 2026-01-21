9e1c760e831a73a02385135ae8f59ce3895d6961
Elena <ahiyevichelena@gmail.com>
2026-01-21 11:42:21 +0800

    practice regex dot character and basic preg_replace patterns (review)

--- 14-regex/01_intro.php
+++ 14-regex/01_intro.php
@@ -1,18 +1,52 @@
 <?php
-	/*
+    declare(strict_types=1);
+    
+	/**
 	 * Введение в регулярные выражения в PHP
+     * ⊗ppPmRgInr
+     * 223 of 447
 	 */
-
-	// ⊗ppPmRgInr
-	
+    
+    // preg_replace($pattern, $replacement, $subject);
+    // . — для одного любого символа (буквы, цифры, пробела, знака препинания)
+    
+    /* --- Задание №1 --- */
+    // Шаблон: буква 'a', один любой символ (.), буква 'b'
 	$str = 'ahb acb aeb aeeb adcb axeb';
-	
-	/* ------------- №1 ------------- */
+ 
 	echo preg_replace('#a.b#', '!', $str);
-	//! ! ! aeeb adcb axeb
+    // Результат: ! ! ! aeeb adcb axeb
 	
-	echo PHP_EOL;
-	
-	/* ------------- №2 ------------- */
+	echo '<br>' . PHP_EOL;
+    
+    /* --- Задание №2 --- */
+    // Шаблон: буква 'a', два любых символа (..), буква 'b'
+    
 	echo preg_replace('#a..b#', '!', $str);
-	// ahb acb aeb ! ! !
+    // Результат: ahb acb aeb ! ! !
+    
+    /* --- Задание №3 --- */
+    // Поиск по краям
+    $str = 'aba acca azza a-a a+a a.a';
+    
+    echo preg_replace('#a.a#', '!', $str);
+    
+    echo '<br>' . PHP_EOL;
+    
+    /* --- Задание №4 --- */
+    // Фиксированная длина
+    $str = '123 1234 153 1563 173';
+    
+    echo preg_replace('#1.3#', 'order', $str);
+    
+    echo '<br>' . PHP_EOL;
+    
+    /* --- Задание №5 --- */
+    // Пробелы — это тоже символы
+    $str = 'a b a.b a+b abb';
+    
+    echo preg_replace('#a.b#', '!', $str);
+    
+    echo '<br>' . PHP_EOL;
+    
+    
